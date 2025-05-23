7bf56175aba7ff57a42f7275a189aaa7df2a65fc
VictorPassaro <147890846+VictorPassaro@users.noreply.github.com>
2025-05-23 18:17:51 -0300

    add owner to js

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,7 +93,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // const normalizedBase = base.replace(/\/+$/, '') + '';
 
     // Find all nav and menu links
-    const links = document.querySelectorAll('.nav__link, .menu__link a');
+    const links = document.querySelectorAll('.nav__link, .menu__link a, .owner');
 
     links.forEach(link => {
         const href = link.getAttribute('href');
